78931c34bd6df21768a8b90224a2ee0b62c0255e
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2020-10-16 15:11:08 +0530

    Update and rename strassen to StrassenMatrixMultiplication.c

--- StrassenMatrixMultiplication.c
+++ StrassenMatrixMultiplication.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-  int a[2][2], b[2][2], c[2][2], i, j, p1, p2, p3, p4 , p5, p6, p7,a,b,c,d,e,f,g,h;
+  int a[2][2], b[2][2], c[2][2], i, j, p1, p2, p3, p4 , p5, p6, p7,az,bz,cz,d,e,f,g,h;
   printf("Enter elements of first matrix: ");
   for(i = 0;i < 2; i++)
   {
@@ -20,21 +20,21 @@ void main()
       }
   }
 
-  a=a[0][0];
-  b=a[0][1];
-  c=a[1][0];
+  az=a[0][0];
+  bz=a[0][1];
+  cz=a[1][0];
   d=a[1][1];
   e=b[0][0];
   f=b[0][1];
   g=b[1][0];
   h=b[1][1];
-  p1=a*(f-h);
-  p2=(a+b)*h;
-  p3=(c+d)*e;
+  p1=az*(f-h);
+  p2=(az+bz)*h;
+  p3=(cz+d)*e;
   p4=d*(g-e);
-  p5=(a+d)*(e+h);
-  p6=(b-d)*(g+h);
-  p7=(a-c)*(e+f);
+  p5=(az+d)*(e+h);
+  p6=(bz-d)*(g+h);
+  p7=(az-cz)*(e+f);
   
   c[0][0] = p5+p4-p2+p6;
   c[0][1] = p1+p2;
